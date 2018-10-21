75af4bc7fafb9f88ff2bb926c3a11c0c651a7e86
L-e-x-o-n <44340857+L-e-x-o-n@users.noreply.github.com>
2018-10-22 00:59:06 +0200

    Allow player to move

--- main.lua
+++ main.lua
@@ -1,31 +1,21 @@
 function love.load()
-x = 100
-y = 100
-speed = 100
+  x = 100
+  y = 100
+  speed = 100
 end
 
 function love.update(dt)
 --You place code here when you update things in the game.
-if love.keyboard.isDown("w") then
-	y = y - speed * dt
-	end
-if love.keyboard.isDown("a") then
-	x = x - speed * dt
-	end
-if love.keyboard.isDown("d") then
-	x = x + speed * dt
-	end
-if love.keyboard.isDown("s") then
-	y = y + speed * dt
-	end
+
+  if love.keyboard.isDown("w") then y = y - speed * dt end
+  if love.keyboard.isDown("a") then x = x - speed * dt end
+  if love.keyboard.isDown("d") then x = x + speed * dt end
+  if love.keyboard.isDown("s") then y = y + speed * dt end
 end
 
 function love.draw()
 --You place code here when you want to draw to the screen.
-love.graphics.print("Pitch Black OOOOH SPOOKI", 400, 300)
-love.graphics.rectangle("line", x, y, 50, 50)
 
-
-
-
+  love.graphics.print("Pitch Black OOOOH SPOOKI", 400, 300)
+  love.graphics.rectangle("line", x, y, 50, 50)
 end
