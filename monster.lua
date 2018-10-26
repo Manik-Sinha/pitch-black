f65e2dcb7f806fc4126686cd063fa9ce664577ba
L-e-x-o-n <44340857+L-e-x-o-n@users.noreply.github.com>
2018-10-26 23:49:50 +0200

    Renamed to monster.lua, fixed typo and put indention

--- monster.lua
+++ monster.lua
@@ -2,7 +2,7 @@ function newmonster(type, health, attack, defense, vision, speed, range)
   local self = {}
   function self.set_xy(xx,yy)
     x = xx
-	y = yy
+    y = yy
   end
   function self.get_type() return type end
   function self.set_health(hp) health = hp end
@@ -11,7 +11,7 @@ function newmonster(type, health, attack, defense, vision, speed, range)
   function self.get_attack() return attack end
   function self.set_defense(def) defense = def end
   function self.get_defense() return defense end
-  function self.set_vison(vis) vision = vis end
+  function self.set_vision(vis) vision = vis end
   function self.get_vision() return vision end
   function self.set_range(ran) range = ran end
   function self.get_range() return range end
@@ -20,8 +20,14 @@ function newmonster(type, health, attack, defense, vision, speed, range)
   function self.get_xy() return x, y end
   function self.get_x() return x end
   function self.get_y() return y end
-  if type == "skeleton" then health, attack, defense, speed, vision = 1, 2, 0, 2, 5 end
-  if type == "spider" then health, attack, defense, speed, vision = 3, 5, 2, 2, 7 end
-  if type == "bloater" then health, attack, defense, speed, vision = 5, 10, 5, 0.5, 4 end
+  if type == "skeleton" then
+    health, attack, defense, speed, vision = 1, 2, 0, 2, 5
+  end
+  if type == "spider" then
+    health, attack, defense, speed, vision = 3, 5, 2, 2, 7
+  end
+  if type == "bloater" then
+    health, attack, defense, speed, vision = 5, 10, 5, 0.5, 4
+  end
   return self
 end
